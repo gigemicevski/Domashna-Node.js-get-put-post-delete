0f61bfc736f6bef5e148d1326e4930d2bdce87f3
Gligor <52067606+gigemicevski@users.noreply.github.com>
2019-10-15 20:47:03 +0200

    Add files via upload

--- web-api/app.js
+++ web-api/app.js
@@ -2,7 +2,11 @@ var express = require('express');
 require('dotenv/config');
 var bodyParser = require('body-parser');
 const fs = require('fs');
-var path = require('path'); 
+var path = require('path');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); 
 
 
 //read
@@ -13,59 +17,61 @@ console.log(users);
 //write//write
 
 let addUser = { 
-    name: 'Mike',
-    surname: 'Michaels', 
+    name: 'Gligor',
+    surname: 'Michevski', 
     email: 'Male@madms.com',
-    age: 23,
+    age: 32,
     isActive: true
 };
- 
-let data = JSON.stringify(addUser, null, 2);
-fs.writeFileSync('.json', data);
+// let data = JSON.stringify(addUser, null, 2);
+// fs.writeFileSync('web-api/.json', data);
+
+fs.writeFile('web-api/.json', JSON.stringify(addUser,null,2), (err) => {
+        if (err) console.log('Error writing file:', err)
+    });
 
 
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 app.get('/users', (req, res) => {
     res.status(200).send(users);
 });
 
-app.get('/users/:name', (req, res) => {
-    const found = users.some(user => user.name === req.params.name); //The some() method checks if any of the elements in an array pass a test (provided as a function).
+app.get('/users/:id', (req, res) => {
+    const found = users.some(user => user.id === parseInt(req.params.id)); //The some() method checks if any of the elements in an array pass a test (provided as a function).
     if(found){
-        res.send(users.filter(users => users.name === req.params.name));
+        res.send(users.filter(users => users.id === parseInt(req.params.id)));
     }
     else{
-        res.status(400).send({ msg:`No member with name ${req.params.name}`});
+        res.status(400).send({ msg:`No member with name ${parseInt(req.params.id)}`});
     }
 });
 
 app.post('/users', (req, res) => {
     const newUser = {
-        
+            "id": req.body.id,
             "name": req.body.name,
             "surname": req.body.surname,
             "email": req.body.email,
             "age": req.body.age,
             "isActive":req.body.isActive,
            };
-    if(!newUser.name){
-        return res.status(400).json({msg:"Please enter the name"});
+    if(!newUser.id){
+        return res.status(400).json({msg:"Please enter your id"});
     }
     users.push(newUser);
+    let data = JSON.stringify(users, null, 2);
+    fs.writeFileSync('web-api/users.json', data);
     res.json(users);
 });
 
-app.put('/users/:name', (req, res) => {
-    const found = users.some(user => user.name === req.params.name); //The some() method checks if any of the elements in an array pass a test (provided as a function).
+app.put('/users/:id', (req, res) => {
+    const found = users.some(user => user.id === parseInt(req.params.id)); //The some() method checks if any of the elements in an array pass a test (provided as a function).
     
     if(found){
        const updUser = req.body;
        users.forEach(user =>{
-           if(user.name === req.params.name){
+           if(user.id === parseInt(req.params.id)){
+            user.id = updUser.id ? updUser.id : user.id;
             user.name = updUser.name ? updUser.name : user.name;
             user.surname = updUser.surname ? updUser.surname : user.surname;        //if thats included //if the name is sent with request
             user.email = updUser.email ? updUser.email : user.email;;
@@ -73,27 +79,53 @@ app.put('/users/:name', (req, res) => {
             user.isActive = updUser.isActive ? updUser.isActive : user.isActive;
 
             res.json({msg:'user updated', user}); 
+            let data = JSON.stringify(users, null, 2);
+             fs.writeFileSync('web-api/users.json', data);
            }
        });
     }
     else{
-        res.status(400).send({ msg:`No member with name ${req.params.name}`});
+        res.status(400).send({ msg:`No member with this id ${parseInt(req.params.id)}`});
     }
 });
 
-app.patch('/users/:name', (req, res) => {
-    res.send("Partial update for user with id = " + req.params.id);
-});
-
-app.delete('/users/:name', (req, res) => {
-    const found = users.some(user => user.name === req.params.name); 
+app.patch('/users/:id', (req, res) => {
+    const found = users.some(user => user.id === parseInt(req.params.id)); //The some() method checks if any of the elements in an array pass a test (provided as a function).
+    
     if(found){
+       const updUser = req.body;
+       users.forEach(user =>{
+           if(user.id === parseInt(req.params.id)){
+            user.id = updUser.id ? updUser.id : user.id;
+            user.name = updUser.name ? updUser.name : user.name;
+            user.surname = updUser.surname ? updUser.surname : user.surname;        //if thats included //if the name is sent with request
+            user.email = updUser.email ? updUser.email : user.email;;
+            user.age = updUser.age ? updUser.age : user.age;
+            user.isActive = updUser.isActive ? updUser.isActive : user.isActive;
+
+            res.json({msg:'user updated', user}); 
+            let data = JSON.stringify(users, null, 2);
+             fs.writeFileSync('web-api/users.json', data);
+           }
+       });
+    }
+    else{
+        res.status(400).send({ msg:`No member with this id ${parseInt(req.params.id)}`});
+    }
+});
+app.delete('/users/:id', (req, res) => {
+    const found = users.some(user => user.id === parseInt(req.params.id)); 
+    if(found){
+        users = users.filter(users => users.id !== parseInt(req.params.id));
+        let data = JSON.stringify(users, null, 2);
+        fs.writeFileSync('web-api/users.json', data);
         res.json({
             msg:'User Deleted',
-            users: users.filter(users => users.name !== req.params.name)
-        });
+            users: users.filter(users => users.id !== parseInt(req.params.id)),
+            });
+        
 }   else { 
-        res.status(400).send({ msg:`No member with name ${req.params.name}`});
+        res.status(400).send({ msg:`No member with this id ${parseInt(req.params.id)}`});
     }
 });
 
@@ -103,26 +135,3 @@ app.listen(port, () => {
 });
 
 
-// var num = function(){
-//     for (let i = 0; i < 6; i++) {
-//         const element = i+1;
-//         console.log(element);
-//         return element;
-//     }
-// }
-// num();
-
-// var usersId = users.map((obj) => {
-//     return { 
-//         id:'',
-//         name: obj.name,
-//         surname: obj.surname,
-//         email: obj.email,
-//         age: obj.age,
-//         isActive: obj.isActive
-      
-//       };
-      
-//    });
-
-//    console.log(usersId);
